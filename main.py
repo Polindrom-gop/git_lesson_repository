89b1229fc0bfdb2541fc25ce84a495545b6be0b0
Студент <polinastarkova084@gmail.com>
2023-11-24 17:32:53 +0300

    Последняя версия

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-# I am author!
-print("My first Git program!!!")
-# 2017 (c) Me
-print("Hello, python")
+def my_superfunction():
+    print('What an awesome print!')
+
+
+def main():
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+    my_superfunction()
+
+
+if __name__ == '__main__':
+    main()
